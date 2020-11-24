d57f602a9b37c52efa6adee954bc992111e37d45
baker615512 <baker615512@gmail.com>
2020-11-24 17:11:59 -0600

    seeded database

--- stylish_shades_api/db/seeds.rb
+++ stylish_shades_api/db/seeds.rb
@@ -1,24 +1,93 @@
-sunglasses = [
-  ["B1000", 200, 2, "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_PNG154.png", 20],
-  ["A5000", 270, 1, "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_A5000.png", 20],
-  ["A5100", 180, 1, "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_A5100.png", 20],
-  ["A5200", 150, 1, "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_A5200.png", 20],
-  ["A5300", 140, 1, "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_A5300.png", 20],
-  ["A5400", 390, 1, "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_A5400.png", 20],
-  ["A5500", 450, 1, "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_A5500.png", 20],
-  ["A5600", 520, 1, "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_A5600.png", 20],
-  ["A5700", 240, 1, "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_A5700.png", 20],
-  ["A5800", 330, 1, "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_A5800.png", 20],
-  ["B1100", 100, 2, "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_B1100.png", 20],
-  ["B1200", 150, 2, "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_B1200.png", 20],
-  ["B1300", 470, 2, "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_B1300.png", 20],
-  ["B1500", 200, 2, "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_B1500.png", 20],
-  ["B1600", 110, 2, "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_B1600.png", 20],
-  ["B1700", 310, 2, "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_B1700.png", 20],
-  ["B1800", 160, 2, "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_B1800.png", 20],
-  ["B1900", 220, 2, "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_B1900.png", 20]
-]
+Category.create(name: "Casual")
+Category.create(name: "Sporty")
 
-sunglasses.each do |model, price, category_id, image, stock_quantity|
-  Sunglass.create(model: model, price: price, category_id: category_id, image: image, stock_quantity: stock_quantity)
-end
+Sunglass.create(model: "B1000",
+  price: 200,
+  category_id: 2,
+  image: "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_PNG154.png",
+  stock_quantity: 20)
+Sunglass.create(model: "A5000",
+  price: 270,
+  category_id: 1,
+  image: "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_A5000.png",
+  stock_quantity: 20)
+Sunglass.create(model: "A5100",
+  price: 180,
+  category_id: 1,
+  image: "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_A5100.png",
+  stock_quantity: 20)
+Sunglass.create(model: "A5200",
+  price: 150,
+  category_id: 1,
+  image: "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_A5200.png",
+  stock_quantity: 20)
+Sunglass.create(model: "A5300",
+  price: 140,
+  category_id: 1,
+  image: "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_A5300.png",
+  stock_quantity: 20)
+Sunglass.create(model: "A5400",
+  price: 390,
+  category_id: 1,
+  image: "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_A5400.png",
+  stock_quantity: 20)
+Sunglass.create(model: "A5500",
+  price: 450,
+  category_id: 1,
+  image: "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_A5500.png",
+  stock_quantity: 20)
+Sunglass.create(model: "A5600",
+  price: 520,
+  category_id: 1,
+  image: "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_A5600.png",
+  stock_quantity: 20)
+Sunglass.create(model: "A5700",
+  price: 240,
+  category_id: 1,
+  image: "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_A5700.png",
+  stock_quantity: 20)
+Sunglass.create(model: "A5800",
+  price: 330,
+  category_id: 1,
+  image: "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_A5800.png",
+  stock_quantity: 20)
+Sunglass.create(model: "B1100",
+  price: 100,
+  category_id: 2,
+  image: "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_B1100.png",
+  stock_quantity: 20)
+Sunglass.create(model: "B1200",
+  price: 150,
+  category_id: 2,
+  image: "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_B1200.png",
+  stock_quantity: 20)
+Sunglass.create(model: "B1300",
+  price: 470,
+  category_id: 2,
+  image: "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_B1300.png",
+  stock_quantity: 20)
+Sunglass.create(model: "B1500",
+  price: 200,
+  category_id: 2,
+  image: "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_B1500.png",
+  stock_quantity: 20)
+Sunglass.create(model: "B1600",
+  price: 110,
+  category_id: 2,
+  image: "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_B1600.png",
+  stock_quantity: 20)
+Sunglass.create(model: "B1700",
+  price: 310,
+  category_id: 2,
+  image: "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_B1700.png",
+  stock_quantity: 20)
+Sunglass.create(model: "B1800",
+  price: 160,
+  category_id: 2,
+  image: "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_B1800.png",
+  stock_quantity: 20)
+Sunglass.create(model: "B1900",
+  price: 220,
+  category_id: 2,
+  image: "https://sunglassesforjsproject.s3.us-east-2.amazonaws.com/sunglasses_B1900.png",
+  stock_quantity: 20)
