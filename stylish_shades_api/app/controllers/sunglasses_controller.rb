c89cd986f4cf6c297693c3e4384fbc38260ba12e
baker615512 <baker615512@gmail.com>
2020-11-25 11:13:53 -0600

    added sunglasses controller actions

--- stylish_shades_api/app/controllers/sunglasses_controller.rb
+++ stylish_shades_api/app/controllers/sunglasses_controller.rb
@@ -1,9 +1,31 @@
 class SunglassesController < ApplicationController
+  before_action :set_sunglass, only: [:show, :update]
 
   def index
     @sunglasses = Sunglass.all 
-    render json: @sunglasses 
+    render json: @sunglasses, except: [:created_at, :updated_at]
   end
 
+  def show
+    render json: @sunglass
+  end
+
+  def update
+    if @sunglass.update(sunglass_params)
+      render json: @sunglass
+    else 
+      render json: @sunglass.errors
+    end
+  end
+
+  private
+
+  def sunglass_params
+    params.require(:sunglass).permit(:model, :price, :category_id, :image, :stock_quantity)
+  end
+
+  def set_sunglass
+    @sunglass = Sunglass.find_by_id(params[:id])
+  end
 
 end
