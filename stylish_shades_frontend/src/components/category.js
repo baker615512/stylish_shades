09311fc795da8817b2c4b46b13b662f596ffa772
baker615512 <baker615512@gmail.com>
2020-11-25 14:04:34 -0600

    adding buttons and event listener

--- stylish_shades_frontend/src/components/category.js
+++ stylish_shades_frontend/src/components/category.js
@@ -4,14 +4,31 @@ class Category {
   constructor(category){
     this.category = category
     this.render()
+    this.attachEventListener()
   }
 
   static getAll(){
-    api.getAllCategories().then((data) => 
+    api.getAllCategories().then((data) =>
       data.forEach((category) => new Category(category)))
   }
 
   render(){
-    console.log("Do I make it to here?")
+    // include the category id as a data attribute
+    const btn = document.createElement("button")
+    btn.className = "category-button"
+    btn.innerHTML = this.category.name
+    this.constructor.container.appendChild(btn)
+    this.btn = btn
   }
+
+  attachEventListener = () => {
+    this.btn.addEventListener("click", this.handleOnClick)
+  }
+
+  handleOnClick = () => {
+    console.log("clicked")
+    //get the category id from the data-id attribute
+    //pass the id into a method on apiService to make a request to categories/:id/sunglasses
+  }
+
 }
